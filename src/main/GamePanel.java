de5dfa118bae183ab3ba7b3615ae0afe7113c949
kev-fbx <ktran041203@gmail.com>
2025-02-03 22:11:23 +1100

    Initial commit

---
+++ src/main/GamePanel.java
@@ -0,0 +1,7 @@
+package src.main;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+    final int TILE_SIZE = 16;
+}
---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package src.main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("A Wonderful World!");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
