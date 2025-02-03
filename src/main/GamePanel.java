37d34a821282d35a921ca8614a94dabae40bb597
kev-fbx <ktran041203@gmail.com>
2025-02-03 22:26:51 +1100

    Implemented game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 // Defines Panel for GUI
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     final int ORG_TILE_SIZE = 16;
     final int SCALE = 3;
@@ -17,9 +17,23 @@ public class GamePanel extends JPanel {
     final int SCREEN_WIDTH = TILE_SIZE * MAX_COL;
     final int SCREEN_HEIGHT = TILE_SIZE * MAX_ROW;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.LIGHT_GRAY);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            System.out.println("Test");
+        }
     }
 }
